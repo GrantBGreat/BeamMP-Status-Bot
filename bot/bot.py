25f1339b4fdbf891b9543ca4b86a3b13f9dcfecf
Grant <grant@bourneadvisory.ca>
2020-12-05 18:03:54 -0500

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -8,9 +8,20 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='!', description="The bot for all your BeamMP needs.", help_command = None)
+bot = commands.Bot(command_prefix='!', description="The bot for all your BeamMP needs.", help_command = None, case_insensitive = True)
 
-@bot.command() #help command
+print("Bot is starting...")
+
+@bot.event
+async def on_ready():
+    print('\n\nLogged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('-------------------')
+
+#########################################COMMANDS#####################################################################
+
+@bot.command(name="help", description = "Learn What each command does.") #help command
 async def help(ctx, args=None):
     help_embed = discord.Embed(title="BeamMP Status Commands:", color = 0x8a3f0a)
     command_names_list = [x.name for x in bot.commands]
@@ -45,18 +56,20 @@ async def help(ctx, args=None):
     await ctx.send(embed=help_embed)
 
 
-#@bot.command()
-#def Set():
-#    return
+@bot.command(name = "save", description = "Can be run by admins only.\n\nThis command sets the server that can be reached by doing the `!check` command\n\n**Impemetation:**\n`!save <ip>:<port>")
+@commands.has_permissions(administrator=True)
+async def save(ctx, server_ip, server_port):
+    save_embed = discord.Embed(title="Server Save:", color = 0x8a3f0a)
+
+    if 1 == 0: #must add implemetation
+        save_embed.add_field(name='success!', value="The discord's BeamMP server has been set to:\n{server_ip}:{server_port}")
+    else:
+        save_embed.add_field(name='ERROR', value="Please enter a valid ip and port\n`!save <ip>:<port>`")
+
+    await ctx.send(embed=save_embed)
+
 
 ######################################################################################################################
-
-@bot.event
-async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('-------------------')
 
 
 bot.run(TOKEN)
